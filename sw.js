183d249b264c970de49611a81ed1f4f10fda051a
Ekilo <makisekurisuovo@gmail.com>
2024-10-27 08:40:21 +0800

    Site updated: 2024-10-27 08:40:19

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1729989171054";
+const VERSION = "1729989609958";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
